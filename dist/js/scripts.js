9fe000edb506ae52f6b65cb7ba22253ad997da40
mtr0930 <71540277+mtr0930@users.noreply.github.com>
2021-08-22 01:28:49 +0900

    Update scripts.js

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -51,17 +51,15 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
     
-    var dday = new Date("November 30, 2020, 0:00:00").getTime();
+    var dday = new Date("April 10, 2020, 0:00:00").getTime();
 
     setInterval(function() {
     var today = new Date().getTime();
-    var gap = dday - today;
+    var gap = today - dday;
     var day = Math.ceil(gap / (1000 * 60 * 60 * 24));
-    var hour = Math.ceil((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var min = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60));
-    var sec = Math.ceil((gap % (1000 * 60)) / 1000);
 
-    document.getElementById("count").innerHTML = "D-DAY까지 " + day + "일 " + hour + "시간 " + min + "분 " + sec + "초 남았습니다.";
+
+    document.getElementById("count").innerHTML = "우리가 만난날은 " + day + "일 ";
     }, 1000);
 
 });
